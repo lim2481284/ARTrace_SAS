20c7d1119c0e215dd27db217adcaa9cde5a55f1c
Nik Mirza <nik96mirza@gmail.com>
2018-04-26 12:24:41 +0800

    Change to use Subject_Id

--- face-recog/js/faceRecognition.js
+++ face-recog/js/faceRecognition.js
@@ -6,7 +6,9 @@ var headers = {
 let subject = {
   id:null,
   face_id:"",
-  profile:null
+  profile:null,
+  recommendation:"",
+  emotion:[]
 }
 
 function faceReco(image){
@@ -26,7 +28,7 @@ function faceReco(image){
         console.log(response);
         if (response.images[0].transaction.status=="success"){
             console.log("Face matches")
-            subject.face_id=response.images[0].transaction.face_id;
+            subject.face_id=response.images[0].transaction.subject_id;
 
             checkFace(subject.face_id).done(function(res){
 
@@ -88,21 +90,25 @@ function faceEnroll(image,id=null){
         console.log(response)
 
         if(response.face_id){
-          subject.face_id=response.face_id;
+          subject.face_id=response.images[0].transaction.subject_id;
             //Here need to bind the faceid to current
           checkFace(subject.face_id).done(function(res){
 
             if(res.length>0){
               // Customer Exist
+              console.log("Exist Respond : ",res);
               subject.id = res[0].id;
               subject.face_id = res[0].face_id;
               subject.profile = res[0].profile;
+              updateInfo();
             }else{
               // New Customer add to Database
               addCustomer(subject.face_id).done(function(resp){
+                console.log("Add Respond : ",resp);
                 subject.id = resp.id;
                 subject.face_id = resp.face_id;
                 subject.profile = resp.profile;
+                updateInfo();
               });
             }
 
@@ -112,13 +118,27 @@ function faceEnroll(image,id=null){
 }
 
 // Database API
-function addCustomer(face_id, profile = {}){
+let db_url = "http://localhost:3000"//"http://sas.windworkshop.my";
+
+function addCustomer(face_id, profile = {
+  "name":"New Customer",
+  "house_loan":"2018-03-31 09:57:00",
+  "car_loan":null,
+  "acc_balance":20000.00,
+  "CTOS":600,
+  "income":5000,
+  "age":51,
+  "occupation":2,
+  "spending_history":1
+}, emotion = []){
+
   var payload = {
     face_id:face_id,
-    profile:profile
+    profile:profile,
+    emotion:emotion
   };
 
-  return $.ajax("http://sas.windworkshop.my/customer", {
+  return $.ajax(db_url+"/customer", {
     type:"POST",
     data:JSON.stringify(payload),
     dataType: "json",
@@ -126,13 +146,16 @@ function addCustomer(face_id, profile = {}){
   })
 }
 
-function editCustomer(id, face_id, profile = {}){
+function editCustomer(id = subject.id, face_id = subject.face_id,
+  profile = subject.profile,emotion = subject.emotion){
+
   var payload = {
     face_id:face_id,
-    profile:profile
+    profile:profile,
+    emotion:emotion
   };
 
-  return $.ajax("http://sas.windworkshop.my/customer/"+id, {
+  return $.ajax(db_url+"/customer/"+id, {
     type:"PUT",
     data:JSON.stringify(payload),
     dataType: "json",
@@ -141,7 +164,7 @@ function editCustomer(id, face_id, profile = {}){
 }
 
 function getCustomer(id){
-  return $.ajax("http://sas.windworkshop.my/customer/"+id, {
+  return $.ajax(db_url+"/customer/"+id, {
     type:"GET",
     dataType: "json",
     contentType:"application/json"
@@ -149,9 +172,19 @@ function getCustomer(id){
 }
 
 function checkFace(face_id){
-  return $.ajax("http://sas.windworkshop.my/post?face_id_like="+id, {
+  return $.ajax(db_url+"/customer?face_id_like="+face_id, {
     type:"GET",
     dataType: "json",
     contentType:"application/json"
   });
+}
+
+// Recommendation formula
+function getRecommendation(){
+}
+
+function updateInfo(){
+  $(".info_name").html(subject.profile.name);
+  $(".info_age").html(subject.profile.age);
+  //$(".info_name").html(subject.profile.name);
 }
